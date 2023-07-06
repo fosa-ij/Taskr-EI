5c53dec1db8fb06f06eb21b1341c6b7405914d91
fosa-ij <efosaij@gmail.com>
2023-07-06 21:34:00 +0100

    updated todo-component

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,33 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import '../Todo.css'
 
 const Todo = () => {
     return ( 
-        <div>
-            <p>A Simple Todo</p>
-            <input type="checkbox" />
-            <button><FontAwesomeIcon icon={faPenToSquare} /></button>
-            <button><FontAwesomeIcon icon={faTrash} /></button>
+        <div className='todo-container'>
+            <div className='display-flex'>
+                <div>
+                    <p>A Simple Todo</p>
+                    <span>Todo Tag</span>
+                </div>
+                <div>
+                    <input type="checkbox" />   
+                </div>
+            </div>
+            <hr />
+            <div className='display-flex'>
+                <div>
+                    <span>Today</span>
+                    <span>11:00PM</span>
+                </div>
+                <div>
+                    <button><FontAwesomeIcon icon={faPenToSquare} /></button>
+                    <button><FontAwesomeIcon icon={faTrash} /></button>
+                </div>
+            </div>
         </div>
-     );
+    );
 }
  
 export default Todo;
