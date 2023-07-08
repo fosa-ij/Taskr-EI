d4bcc5990ec58b807b26c794e6c71cca337184cc
Aisosa <allysonaghedo@gmail.com>
2023-07-08 06:51:36 +0100

    Added a 'delete todo' functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import './App.css'
 import Input from './components/Input'
-import { useState, createContext } from 'react'
+import { useState, createContext, useContext } from 'react'
 
 export const CheckContext = createContext()
+export const CheckTodo = createContext()
 
 function App() {
+  const storedTodos = JSON.parse(localStorage.getItem("todos"));
   const [isChecked, setIsChecked] = useState(false)
+  const [todoList, setTodoList] = useState(storedTodos || "[]");
 
   return (
     <>
       <CheckContext.Provider value={[isChecked, setIsChecked]}>
-        <h1>Todo List</h1>
-        <Input />
+        <CheckTodo.Provider value={[todoList, setTodoList]}>
+          <h1>Todo List</h1>
+          <Input />
+        </CheckTodo.Provider>
       </CheckContext.Provider>
     </>
-  )
+  );
 }
 
 export default App
