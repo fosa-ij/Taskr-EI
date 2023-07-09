c25d59697a241ac99445552391e97e06f2f2138a
Oyase Emmanuel <114712121+emmykhalifa@users.noreply.github.com>
2023-07-09 07:22:41 -0700

    Add files via upload

---
+++ src/component/signUp.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+    const SignUp = () => {
+        const [username, setUsername] = useState ('')
+        const [email, setEmail] =  useState('')
+        const [password, setPassword] = useState('')
+
+const handleSubmit = (event)=> {
+    event.preventDefault();
+};
+
+/*Handle Form Submission */
+
+  return (
+    <div className="signup">
+        <h1>Sign Up Page</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">
+                Username:
+            </label>
+            <input type="text" 
+            id="username"
+            name="username-input" 
+            value= {username} 
+            placeholder='Username' 
+            onChange={(event) => setUsername(event.target.value)} required/>
+            <br />
+            <label className="email" htmlFor="email">
+                Email:                         
+            </label>
+            <input type="text" 
+            name="email-input" 
+            placeholder="example@gmail.com" 
+            value='' 
+            id="email"
+            onChange={(event) => (event.target.value)} required/>
+            <br />
+            <label htmlFor="password">
+                Password:
+            </label>
+            <input type="password"
+            name="password-input" 
+            id="password"
+            placeholder="Password" 
+            onChange={(event) => (event.target.value)} required/>
+            <br />
+            <button type="submit">Sign Up</button>
+        </form>
+    </div>
+  )
+}
+
+export default SignUp;
