60ec718ca758a8b51e28945f311945106d6863c9
fosa-ij <efosaij@gmail.com>
2023-07-10 06:41:13 +0100

    version 1.1.23

--- src/components/signup-page.jsx
+++ src/components/signup-page.jsx
@@ -1,55 +1,71 @@
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-    const SignUp = () => {
-        const [username, setUsername] = useState ('')
-        const [email, setEmail] =  useState('')
-        const [password, setPassword] = useState('')
+const SignUp = () => {
+    const [username, setUsername] = useState ('')
+    const [email, setEmail] =  useState('')
+    const [password, setPassword] = useState('')
+    const [cPassword, setCPassword] = useState('')
+    const navigate = useNavigate()
 
 const handleSubmit = (event)=> {
     event.preventDefault();
+    navigate('/login')
+    
 };
 
 /*Handle Form Submission */
 
   return (
-    <div className="signup">
-        <h1>Sign Up Page</h1>
-        <form onSubmit={handleSubmit}>
-            <div className="input-field">
-                <label htmlFor="username">
-                    Username:
-                </label>
-                <input className="username" type="text" 
-                    id="username"
-                    name="username-input" 
-                    value= {username} 
-                    placeholder='Username' 
-                    onChange={(event) => setUsername(event.target.value)} required/>
-                <label className="email" htmlFor="email">
-                    Email:                         
-                </label>
-                <input type="text" 
-                    name="email-input" 
-                    placeholder="example@gmail.com" 
-                    value='' 
-                    id="email"
-                    onChange={(event) => (event.target.value)} required/>
-                
-                <label htmlFor="password">
-                    Password:
-                </label>        <br />
-                <input type="password"
-                    name="password-input" 
-                    id="password"
-                    placeholder="Password" 
-                    onChange={(event) => (event.target.value)} required/>
-                <br />
-                </div>
-                <button type="submit">Sign Up</button>
-                
-        </form>
+    <div className="main-container" id="signup">
+      <h1>Sign Up </h1>
+      <form onSubmit={handleSubmit}>
+        <div className="input-field">
+          <input
+            className="username"
+            type="text"
+            id="username"
+            name="username-input"
+            value={username}
+            placeholder="Username"
+            onChange={(event) => setUsername(event.target.value)}
+            required
+          />
+
+          <input
+            type="text"
+            name="email-input"
+            placeholder="example@gmail.com"
+            value={email}
+            id="email"
+            onChange={(event) => setEmail(event.target.value)}
+            required
+          />
+          <input
+            type="password"
+            name="password-input"
+            id="password"
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+            required
+          />
+          <input
+            type="password"
+            name="password-input"
+            id="cpassword"
+            placeholder=" Confirm Password"
+            onChange={(event) => setCPassword(event.target.value)}
+            required
+          />
+          <br />
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+      <p>
+        Already signed up? <Link className="login" to="/login">Login</Link>{" "}
+      </p>
     </div>
-  )
+  );
 }
 
 export default SignUp;
